8aaea0d79fc219eaf48246ca1cd0b16962c86851
Tarcio Diniz <tarciodiniz0@gmail.com>
2025-01-24 21:08:10 -0300

    fix: corrige styles global

--- src/app/shared/components/userRegistration/index.tsx
+++ src/app/shared/components/userRegistration/index.tsx
@@ -1,202 +1,197 @@
-import DialogAttention from '@shared/dialogs/dialog-attention/dialog-attention';
-import DialogEditUser from '@shared/dialogs/dialog-editUser';
 import DialogUserRegistration from '@shared/dialogs/dialog-new-user';
-import { Button, Checkbox, Form, Input, message, Modal, Select, Space, Table, TableColumnsType, TableProps, } from 'antd';
-import { SearchProps } from 'antd/es/input';
-import form from 'antd/lib/form';
-import React, { useState } from 'react';
+import {Button, Form, Input, Modal, Space, Table, TableColumnsType, TableProps,} from 'antd';
+import React, {useState} from 'react';
 
 type TableRowSelection<T extends object = object> =
-    TableProps<T>["rowSelection"];
+  TableProps<T>["rowSelection"];
 
 interface DataType {
-    key: React.Key;
-    id: number;
-    age: number;
-    telefone: number;
-    status: string;
-    email: string;
-    user: string;
-    create: string;
-    lastAlteration: string;
+  key: React.Key;
+  id: number;
+  age: number;
+  telefone: number;
+  status: string;
+  email: string;
+  user: string;
+  create: string;
+  lastAlteration: string;
 }
 
 const columns: TableColumnsType<DataType> = [
-    {
-        title: "ID",
-        dataIndex: "id",
-    },
-    {
-        title: "Razão Social",
-        dataIndex: "age",
-    },
-    {
-        title: "Telefone",
-        dataIndex: "telefone",
-    },
-    {
-        title: "Status",
-        dataIndex: "status",
-    },
-    {
-        title: "E-mail",
-        dataIndex: "email",
-    },
-    {
-        title: "Perfil do Usuário",
-        dataIndex: "user",
-    },
-    {
-        title: "Criado",
-        dataIndex: "create",
-    },
-    {
-        title: "Ultima Alteração",
-        dataIndex: "lastAlteration",
-    },
+  {
+    title: "ID",
+    dataIndex: "id",
+  },
+  {
+    title: "Razão Social",
+    dataIndex: "age",
+  },
+  {
+    title: "Telefone",
+    dataIndex: "telefone",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+  },
+  {
+    title: "E-mail",
+    dataIndex: "email",
+  },
+  {
+    title: "Perfil do Usuário",
+    dataIndex: "user",
+  },
+  {
+    title: "Criado",
+    dataIndex: "create",
+  },
+  {
+    title: "Ultima Alteração",
+    dataIndex: "lastAlteration",
+  },
 ];
 
-const dataSource = Array.from({ length: 46 }).map<DataType>((_, i) => ({
-    key: i,
-    id: 1,
-    age: 32,
-    telefone: 88524226,
-    status: "Ativo",
-    email: "amanda@gmail.com",
-    user: "Admin",
-    create: "00/00/0000",
-    lastAlteration: "00/00/0000",
+const dataSource = Array.from({length: 46}).map<DataType>((_, i) => ({
+  key: i,
+  id: 1,
+  age: 32,
+  telefone: 88524226,
+  status: "Ativo",
+  email: "amanda@gmail.com",
+  user: "Admin",
+  create: "00/00/0000",
+  lastAlteration: "00/00/0000",
 }));
 
-const { Search } = Input;
-
+const {Search} = Input;
 
 
 const UserRegistration: React.FC = () => {
 
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [form] = Form.useForm();
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
 
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        console.log("selectedRowKeys changed: ", newSelectedRowKeys);
-        setSelectedRowKeys(newSelectedRowKeys);
-    };
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const rowSelection: TableRowSelection<DataType> = {
-        selectedRowKeys,
-        onChange: onSelectChange,
-        selections: [
-            Table.SELECTION_ALL,
-            Table.SELECTION_INVERT,
-            Table.SELECTION_NONE,
-            {
-                key: "odd",
-                text: "Select Odd Row",
-                onSelect: (changeableRowKeys) => {
-                    let newSelectedRowKeys = [];
-                    newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                        if (index % 2 !== 0) {
-                            return false;
-                        }
-                        return true;
-                    });
-                    setSelectedRowKeys(newSelectedRowKeys);
-                },
-            },
-            {
-                key: "even",
-                text: "Select Even Row",
-                onSelect: (changeableRowKeys) => {
-                    let newSelectedRowKeys = [];
-                    newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
-                        if (index % 2 !== 0) {
-                            return true;
-                        }
-                        return false;
-                    });
-                    setSelectedRowKeys(newSelectedRowKeys);
-                },
-            },
-        ],
-    };
+  const rowSelection: TableRowSelection<DataType> = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: "odd",
+        text: "Select Odd Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: "even",
+        text: "Select Even Row",
+        onSelect: (changeableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changeableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
 
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const handleOpen = () => setIsModalVisible(true);
-    const handleClose = () => setIsModalVisible(false);
-    const handleConfirm = () => {
-        setIsModalVisible(false);
-        console.log('Dados enviados!');
-    };
-    return (
+  const handleOpen = () => setIsModalVisible(true);
+  const handleClose = () => setIsModalVisible(false);
+  const handleConfirm = () => {
+    setIsModalVisible(false);
+    console.log('Dados enviados!');
+  };
+  return (
 
-        <div>
-            <div
-                style={{
-                    position: "fixed",
-                    top: 80,
-                    right: 0,
-                    left: 0,
-                    zIndex: 1000,
-                    padding: "10px 20px",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    gap: "10px",
-                    borderBottom: "1px #ddd",
-                }}
-            >
-                <Space direction="horizontal">
-                    <Search
-                        placeholder="Pesquisar"
-                        onSearch={(value) => console.log(value)}
-                        style={{ width: 200, }}
-                    />
-                    <Button type="default" onClick={handleOpen}>Editar</Button>
-                    <Button type="primary" danger onClick={openModal}>
-                        Criar Usuário
-                    </Button>
-                </Space>
-            </div>          
-                {/*<DialogEditUser
+    <div>
+      <div
+        style={{
+          position: "fixed",
+          top: 80,
+          right: 0,
+          left: 0,
+          zIndex: 1000,
+          padding: "10px 20px",
+          display: "flex",
+          justifyContent: "flex-end",
+          gap: "10px",
+          borderBottom: "1px #ddd",
+        }}
+      >
+        <Space direction="horizontal">
+          <Search
+            placeholder="Pesquisar"
+            onSearch={(value) => console.log(value)}
+            style={{width: 200,}}
+          />
+          <Button type="default" onClick={handleOpen}>Editar</Button>
+          <Button type="primary" danger onClick={openModal}>
+            Criar Usuário
+          </Button>
+        </Space>
+      </div>
+      {/*<DialogEditUser
                     isVisible={isModalVisible}
                     onClose={handleClose}
                     onConfirm={handleConfirm}
                 />*/}
 
-            <Modal
-                //title="CRIAR NOVO USUÁRIO" 
-                open={isModalOpen}
-                footer={null}
-                onCancel={closeModal}
+      <Modal
+        //title="CRIAR NOVO USUÁRIO"
+        open={isModalOpen}
+        footer={null}
+        onCancel={closeModal}
 
-                style={{ width: '601px', alignItems: 'center', }}
-            >
-                <div style={{ top: '20px' }}>
-                    <DialogUserRegistration
+        style={{width: '601px', alignItems: 'center',}}
+      >
+        <div style={{top: '20px'}}>
+          <DialogUserRegistration
 
-                    />
-                </div>
-            </Modal>
-
-            <div style={{ marginTop: "60px" }}>
-                <Table
-                    rowSelection={rowSelection}
-                    columns={columns}
-                    dataSource={dataSource}
-                />
-            </div>
+          />
         </div>
+      </Modal>
 
-    );
+      <div style={{marginTop: "60px"}}>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={dataSource}
+        />
+      </div>
+    </div>
+
+  );
 };
 
 export default UserRegistration;
--- src/app/shared/dialogs/dialog-new-user/dialog-perfil-tecnico.tsx
+++ src/app/shared/dialogs/dialog-new-user/dialog-perfil-tecnico.tsx
@@ -1,161 +1,165 @@
-import { FormControl, InputAdornment, InputLabel, MenuItem, SelectChangeEvent, TextField } from '@mui/material';
-import { Button, Checkbox, Form, Input, message, Modal, Select, Space, Table, TableColumnsType, TableProps, Typography, } from 'antd';
-import { SearchProps } from 'antd/es/input';
-import form from 'antd/lib/form';
-import React, { useState } from 'react';
+import {InputAdornment, TextField} from '@mui/material';
+import {Button, Form, message, Space, Typography,} from 'antd';
+import React, {useState} from 'react';
 import './dialog.style.css'
 
 
-const DialogUserRegistrationTecnic: React.FC = ({ ...props }) => {
+const DialogUserRegistrationTecnic: React.FC = ({...props}) => {
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [value, setValue] = useState('');
-    const [form] = Form.useForm();
-
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleGeneratePassword = () => {
-        const randomPassword = Math.random().toString(36).slice(-8);
-        form.setFieldsValue({ password: randomPassword });
-    };
-
-    const handleCopyPassword = () => {
-        const password = form.getFieldValue('password');
-        if (password) {
-            navigator.clipboard.writeText(password)
-                .then(() => message.success('Senha copiada para a área de transferência!'))
-                .catch(() => message.error('Falha ao copiar a senha.'));
-        } else {
-            message.warning('Nenhuma senha para copiar.');
-        }
-    };
-
-    const handleSubmit = () => {
-        form.validateFields()
-            .then((values) => {
-                console.log("Form Values:", values);
-                closeModal();
-            })
-            .catch((info) => {
-                console.log("Validate Failed:", info);
-            });
-    };
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    };
-
-    return (
-
-        <div >
-            <div style={{ justifyContent: 'space-between', left: '32px', borderBottom: '1px solid #ddd', width: '100%' }}>
-                <Space >
-                    <Typography style={{ fontWeight: 'bold', fontSize: '16px', color: '#FF0000', height: '50px', width: '200px', marginLeft: '140px', }} >CRIAR NOVO USUÁRIO</Typography>
-                </Space>
-            </div>
-
-            <Form
-                form={form}
-                layout="vertical"
-                initialValues={{
-                    profile: "Cliente",
-                    isActive: true,
-                    password: "Q1234icL3M4",
-                }}
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [value, setValue] = useState('');
+  const [form] = Form.useForm();
 
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-            >
+  const handleGeneratePassword = () => {
+    const randomPassword = Math.random().toString(36).slice(-8);
+    form.setFieldsValue({password: randomPassword});
+  };
 
-                <Space direction="horizontal" style={{ width: "100%" }}>
-                    <TextField
-                        label="Nome"
-                        variant="outlined"
-                        value={value}
-                        onChange={handleChange}
-                        fullWidth
-                        focused
-                        required
-                        className="outlined-input-name"
-                        placeholder="Aaaaaaaaa"
-                        {...props}
-                        style={{ width: '100%', }}
-                    />
-                </Space>
+  const handleCopyPassword = () => {
+    const password = form.getFieldValue('password');
+    if (password) {
+      navigator.clipboard.writeText(password)
+        .then(() => message.success('Senha copiada para a área de transferência!'))
+        .catch(() => message.error('Falha ao copiar a senha.'));
+    } else {
+      message.warning('Nenhuma senha para copiar.');
+    }
+  };
 
-                <Space direction="horizontal" style={{ width: "100%", }}>
-                    <TextField
-                        label="E-mail"
-                        variant="outlined"
-                        value={value}
-                        onChange={handleChange}
-                        fullWidth
-                        focused
-                        required
-                        placeholder="bbbbb@bb.com"
-                        className="outlined-input-contact"
-                        {...props}
-                        style={{ width: '100%', }}
-                    />
-                </Space>
+  const handleSubmit = () => {
+    form.validateFields()
+      .then((values) => {
+        console.log("Form Values:", values);
+        closeModal();
+      })
+      .catch((info) => {
+        console.log("Validate Failed:", info);
+      });
+  };
 
-                <TextField
-                    label="Senha Provisória"
-                    variant="outlined"
-                    value={form.getFieldValue("password")}
-                    onChange={(e) => form.setFieldsValue({ password: e.target.value })}
-                    fullWidth
-                    focused
-                    required
-                    placeholder="Senha Provisória"
-                    className="outlined-input-password"
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <Button
-                                    type="link"
-                                    onClick={handleGeneratePassword}
-                                    style={{
-                                        minWidth: "auto",
-                                        padding: 0,
-                                        left: '235px',
-                                    }}
-                                >
-                                    🔄
-                                </Button>
-                                <Button
-                                    type="link"
-                                    onClick={handleCopyPassword}
-                                    style={{
-                                        minWidth: "auto",
-                                        padding: 0,
-                                        left: '240px'
-                                    }}
-                                >
-                                    📋
-                                </Button>
-                            </InputAdornment>
-                        ),
-                    }}
-                    style={{ width: "220px" }}
-                />
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <Button onClick={closeModal} style={{ backgroundColor: "white", color: "red" }}>
-                        CANCELAR
-                    </Button>
-                    <Button type="primary" onClick={handleSubmit} style={{ backgroundColor: "red" }}>
-                        CRIAR
-                    </Button>
-                </div>
-            </Form>
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+
+    <div>
+      <div style={{justifyContent: 'space-between', left: '32px', borderBottom: '1px solid #ddd', width: '100%'}}>
+        <Space>
+          <Typography style={{
+            fontWeight: 'bold',
+            fontSize: '16px',
+            color: '#FF0000',
+            height: '50px',
+            width: '200px',
+            marginLeft: '140px',
+          }}>CRIAR NOVO USUÁRIO</Typography>
+        </Space>
+      </div>
+
+      <Form
+        form={form}
+        layout="vertical"
+        initialValues={{
+          profile: "Cliente",
+          isActive: true,
+          password: "Q1234icL3M4",
+        }}
 
 
-        </div >
+      >
 
-    );
+        <Space direction="horizontal" style={{width: "100%"}}>
+          <TextField
+            label="Nome"
+            variant="outlined"
+            value={value}
+            onChange={handleChange}
+            fullWidth
+            focused
+            required
+            className="outlined-input-name"
+            placeholder="Aaaaaaaaa"
+            {...props}
+            style={{width: '100%',}}
+          />
+        </Space>
+
+        <Space direction="horizontal" style={{width: "100%",}}>
+          <TextField
+            label="E-mail"
+            variant="outlined"
+            value={value}
+            onChange={handleChange}
+            fullWidth
+            focused
+            required
+            placeholder="bbbbb@bb.com"
+            className="outlined-input-contact"
+            {...props}
+            style={{width: '100%',}}
+          />
+        </Space>
+
+        <TextField
+          label="Senha Provisória"
+          variant="outlined"
+          value={form.getFieldValue("password")}
+          onChange={(e) => form.setFieldsValue({password: e.target.value})}
+          fullWidth
+          focused
+          required
+          placeholder="Senha Provisória"
+          className="outlined-input-password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button
+                  type="link"
+                  onClick={handleGeneratePassword}
+                  style={{
+                    minWidth: "auto",
+                    padding: 0,
+                    left: '235px',
+                  }}
+                >
+                  🔄
+                </Button>
+                <Button
+                  type="link"
+                  onClick={handleCopyPassword}
+                  style={{
+                    minWidth: "auto",
+                    padding: 0,
+                    left: '240px'
+                  }}
+                >
+                  📋
+                </Button>
+              </InputAdornment>
+            ),
+          }}
+          style={{width: "220px"}}
+        />
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+          <Button onClick={closeModal} style={{backgroundColor: "white", color: "red"}}>
+            CANCELAR
+          </Button>
+          <Button type="primary" onClick={handleSubmit} style={{backgroundColor: "red"}}>
+            CRIAR
+          </Button>
+        </div>
+      </Form>
+
+
+    </div>
+
+  );
 };
 
 export default DialogUserRegistrationTecnic;
