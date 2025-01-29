ccf5b83e72cc4fce55777ad9c610ec9649efc9b4
hericles.souza <hericles.souza@stara.com.br>
2025-01-29 15:25:05 -0300

    fea: adjustment main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import {createRoot} from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import './App.css';
-// import App from './App.tsx';
-import DetailsItensNF from "@app/views/private/rgi/processRGI/DetailsItensNF";
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      {/* <App/> */}
-      <DetailsItensNF/>
+      <App/>
     </Router>
   </StrictMode>
 );
