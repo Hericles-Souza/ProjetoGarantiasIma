dbdbd97ffe59553ea2b23ca947dde4f122d98d0b
hericles.souza <hericles.souza@stara.com.br>
2025-02-11 11:01:05 -0300

    Feat: Commit adjustment home

--- src/app/views/private/garantias/screenGarantia.tsx
+++ src/app/views/private/garantias/screenGarantia.tsx
@@ -1,64 +1,62 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
-import {Button, Tag} from 'antd';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Button, Tag } from 'antd';
 import Header from '@shared/components/header/header.tsx';
 import CardCategorias from '@shared/components/card_garantia/card_garantias.tsx';
 import SearchField from '@shared/components/input_search/input_search.tsx';
 import styled from './screenGarantia.module.css';
 import './carouselAnimations.css';
 import './tabGarantia.css';
-import {getGarantiasByStatusAsync} from "@shared/services/GarantiasService.ts";
-import {GarantiasModel} from "@shared/models/GarantiasModel.ts";
+import { getGarantiasByStatusAsync } from "@shared/services/GarantiasService.ts";
+import { GarantiasModel } from "@shared/models/GarantiasModel.ts";
 import {
   converterStatusGarantiaInverso,
   converterStringParaStatusGarantia,
-  GarantiasStatusEnum,
-  GarantiasStatusEnum2
+  GarantiasStatusEnum
 } from "@shared/enums/GarantiasStatusEnum.ts";
-import {useNavigate} from "react-router-dom";
-import { AuthContext } from '@shared/contexts/Auth/AuthContext';
-
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "@shared/contexts/Auth/AuthContext";
 const Garantias: React.FC = () => {
-  const [activeTab, setActiveTab] = useState<string>('garantias');
+  const [activeTab, setActiveTab] = useState<string>('rgi');
   const [filterStatus, setFilterStatus] = useState<string>("todos");
   const [searchTerm, setSearchTerm] = useState<string>('');
   const carouselRef = useRef<HTMLDivElement>(null);
   const [cardData, setCardData] = useState<GarantiasModel[]>();
   const navigate = useNavigate();
-  const context = useContext(AuthContext);
+ 
 
   useEffect(() => {
     const fetchCardData = async () => {
-      
       try {
-        const response = await getGarantiasByStatusAsync(1, 10, GarantiasStatusEnum2.AGUARDANDO_NF_DEVOLUCAO);
+        const response = await getGarantiasByStatusAsync(1, 10, 2);
         const data = await response.data.data;
-        console.log(JSON.stringify(data));
         setCardData(data);
       } catch (error) {
         console.error('Error fetching card data:', error);
       }
     };
+
     fetchCardData();
   }, []);
-
+  const context = useContext(AuthContext);
   const statuses = Object.values(GarantiasStatusEnum);
 
   const handleNext = () => {
     if (carouselRef.current) {
       const container = carouselRef.current;
-      container.scrollBy({left: 150, behavior: 'smooth'});
+      container.scrollBy({ left: 150, behavior: 'smooth' });
     }
   };
 
   const handlePrevious = () => {
     if (carouselRef.current) {
       const container = carouselRef.current;
-      container.scrollBy({left: -150, behavior: 'smooth'});
+      container.scrollBy({ left: -150, behavior: 'smooth' });
     }
   };
 
   const filteredItems = cardData?.flatMap((card) =>
     card.itens.filter((item) => {
+
       const matchesStatus = filterStatus === 'todos' || item.codigoStatus === converterStatusGarantiaInverso(converterStringParaStatusGarantia(filterStatus));
       const matchesSearch = searchTerm === '' ||
         item.rgi.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -72,8 +70,9 @@ const Garantias: React.FC = () => {
 
   return (
     <>
-      <Header filterStatus={activeTab} handleFilterChange={setActiveTab}/>
-      {activeTab === 'garantias' && (
+      <Header filterStatus={activeTab} handleFilterChange={setActiveTab} />
+
+      {activeTab === 'rgi' && (
         <div className={styled.container}>
           <div className={styled.content}>
             <div ref={carouselRef} className="carousel-container">
@@ -90,14 +89,60 @@ const Garantias: React.FC = () => {
                 ))}
               </div>
             </div>
-            <div style={{display: 'flex', alignItems: 'center', gap: "1rem", padding: "1rem", paddingLeft: "0"}}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: "1rem", padding: "1rem", paddingLeft: "0" }}>
               <Button className={styled.button} type="default" onClick={handlePrevious}>
                 &lt;
               </Button>
               <Button className={styled.button} type="default" onClick={handleNext}>
                 &gt;
               </Button>
-              <SearchField onSearchChange={setSearchTerm}/>
+              <SearchField onSearchChange={setSearchTerm} />
+            </div>
+          </div>
+          <div className={styled.containerGrid}>
+            {filteredItems.map((item) => {
+              const associatedCardData = cardData?.find(card => card.rgi === item.rgi);
+
+              return (
+                <CardCategorias
+                  key={item.id}
+                  data={new Date(associatedCardData.data)}
+                  GarantiaItem={item}
+                  codigoFormatado={`RGI ${item.rgi}`}
+                  onClick={() => navigate(`/garantias/rgi/${associatedCardData.id}`)}
+                />
+              );
+            })}
+          </div>
+
+
+        </div>
+      )}
+      {activeTab === 'aci' && (
+        <div className={styled.container}>
+          <div className={styled.content}>
+            <div ref={carouselRef} className="carousel-container">
+              <div className="carousel-content">
+                {statuses.map((status) => (
+                  <Tag
+                    key={status}
+                    className={`carousel-tag ${styled.tab}`}
+                    color={filterStatus === status ? 'red' : 'default'}
+                    onClick={() => setFilterStatus((prevStatus) => (prevStatus === status ? 'todos' : status))}
+                  >
+                    {status}
+                  </Tag>
+                ))}
+              </div>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', gap: "1rem", padding: "1rem", paddingLeft: "0" }}>
+              <Button className={styled.button} type="default" onClick={handlePrevious}>
+                &lt;
+              </Button>
+              <Button className={styled.button} type="default" onClick={handleNext}>
+                &gt;
+              </Button>
+              <SearchField onSearchChange={setSearchTerm} />
             </div>
           </div>
           <div className={styled.containerGrid}>
@@ -116,7 +161,7 @@ const Garantias: React.FC = () => {
                       navigate(`/garantias/rgi/${associatedCardData.id}`);
                     else if (context.user.rule.name.includes("tecnico") || context.user.rule.name.includes("supervisor"))
                       navigate(`/garantias/technical-and-supervisor/visor-inital/${associatedCardData.id}`);
-                  } } codigoFormatado={''}                />
+                  }} codigoFormatado={''} />
               );
             })}
           </div>
